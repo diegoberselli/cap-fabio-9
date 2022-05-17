530ae343dd5ccf6a917d9853bce22b071357d158
renandcr <renandamagazine@gmail.com>
2022-05-17 17:42:55 -0300

    feature: Middleware para à manipulação de erros. Foi adicionado os arquivos src/errors/AppError.ts, e src/middlewares/handleError.ts.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
-import "express-async-errors"
+import "express-async-errors";
 import express from "express";
+import { handleError } from "./middlewares/handleError";
 
 const app = express();
 
 app.use(express.json());
+app.use(handleError);
 
 app.listen(3000, () => {
-    console.log("Server Started");
-  });
-  
-  export default app;
+  console.log("Server Started");
+});
+
+export default app;
