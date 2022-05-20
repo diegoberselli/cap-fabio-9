8ab02740348fee4a2d025cb0a440943a4d16e8a9
diegoberselli <diegoberselliz@gmail.com>
2022-05-20 16:41:49 -0300

    fix: data-source file

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,19 +1,29 @@
 import { DataSource } from "typeorm";
 import "dotenv/config";
 
-export const AppDataSource = new DataSource({
-  type: "postgres",
-  url: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production" ?
-      { rejectUnauthorized: false}
-      : false,
-  synchronize: false,
-  logging: true,
-  entities: process.env.NODE_ENV === "production"
-      ? ["dist/entities/*.js"]
-      : ["src/entities/*.ts"],
-  migrations: process.env.NODE_ENV === "production"
-      ? ["dist/migrations/*.js"]
-      : ["src/migrations/*.ts"],
-})
-
+export const AppDataSource =
+  process.env.NODE_ENV === "test"
+    ? new DataSource({
+        type: "sqlite",
+        database: ":memory:",
+        entities: ["src/entities/*.ts"],
+        synchronize: true,
+      })
+    : new DataSource({
+        type: "postgres",
+        url: process.env.DATABASE_URL,
+        ssl:
+          process.env.NODE_ENV === "production"
+            ? { rejectUnauthorized: false }
+            : false,
+        synchronize: false,
+        logging: true,
+        entities:
+          process.env.NODE_ENV === "production"
+            ? ["dist/entities/*.js"]
+            : ["src/entities/*.ts"],
+        migrations:
+          process.env.NODE_ENV === "production"
+            ? ["dist/migrations/*.js"]
+            : ["src/migrations/*.ts"],
+      });
