8dfdd7783b2b3e5c563f458663ee42342b50855f
camila suzuki <camila.suzuki.p@gmail.com>
2022-05-20 22:00:17 -0300

    fix:indexStore throw error

--- src/services/store/indexStore.service.ts
+++ src/services/store/indexStore.service.ts
@@ -1,11 +1,15 @@
 import { AppDataSource } from "../../data-source";
 import { IStoreId } from "../../interfaces/store";
 import { Store } from "../../entities/store.entity";
+import { AppError } from "../../errors/AppError";
 
 export default class IndexStoreService {
   static execute = async ({ id }: IStoreId) => {
     const storeRepository = AppDataSource.getRepository(Store);
     const store = await storeRepository.findOne({ where: { id } });
+    if (!store) {
+      throw new AppError(404, "Store not found");
+    }
     return store;
   };
 }
