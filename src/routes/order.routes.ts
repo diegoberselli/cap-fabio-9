b14e2c1068d6fe0d6aca1a94bd898ee6b7b8d285
diegoberselli <diegoberselliz@gmail.com>
2022-05-24 14:57:52 -0300

    fix: yup routes order

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import OrderController from "../controllers/order.controller";
+import createOrdertValidation from "../validators/order/createOrder.validation";
+import { expressYupMiddleware } from "express-yup-middleware";
 
 const router = Router();
 
 export const orderRouter = () => {
-  router.post("", OrderController.create);
+  router.post(
+    "",
+    expressYupMiddleware({ schemaValidator: createOrdertValidation }),
+    OrderController.create
+  );
   router.get("", OrderController.showAll);
   router.get("", OrderController.show);
   router.get("/update", OrderController.update);
