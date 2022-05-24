d2882c1a2fedc95f033d24ea3641c9138bdb350f
diegoberselli <diegoberselliz@gmail.com>
2022-05-24 10:11:20 -0300

    fix: yup

--- src/validators/store/createStore.validation.ts
+++ src/validators/store/createStore.validation.ts
@@ -6,8 +6,8 @@ const createStoretValidation = {
       yupSchema: yup.object().shape({
         branch: yup.string().required("branch name is required"),
         city: yup.string().required("city name is required"),
-        street: yup.number().required("street name is required"),
-        district: yup.number().required("district name is required"),
+        street: yup.string().required("street name is required"),
+        district: yup.string().required("district name is required"),
         number: yup.number().required("number is required"),
         zipCode: yup.number().required("zipCode number is required"),
         phone: yup.number().required("phone number is required"),
