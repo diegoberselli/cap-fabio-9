dd1127f74dffef0b0334728b1dceb0008c8ac544
camila suzuki <camila.suzuki.p@gmail.com>
2022-05-26 20:18:44 -0300

    feat: decremento de quantidade de produto na hora da order de acordo com o repositorio escolhido pelo cliente

--- src/controllers/depotCD.controller.ts
+++ src/controllers/depotCD.controller.ts
@@ -1,52 +1,56 @@
-import {Request, Response} from 'express';
-import AddProductService from '../services/depotCD/addProduct.service';
-import DeleteDepotCDProductService from '../services/depotCD/deleteProductDepotCD.service';
-import IndexDepotCDProductService from '../services/depotCD/indexDepotCDProduct.service';
-import ListDepotCDService from '../services/depotCD/listDepotCD.service';
-import UpdateDepotCdProductService from '../services/depotCD/updateDepotCDProduct.service';
-import DeleteCDProductService from '../services/storageCD/deleteCDProduct.service';
-import ListCDProductsService from '../services/storageCD/listStorageCD.service';
+import { Request, Response } from "express";
+import AddProductService from "../services/depotCD/addProduct.service";
+import DeleteDepotCDProductService from "../services/depotCD/deleteProductDepotCD.service";
+import IndexDepotCDProductService from "../services/depotCD/indexDepotCDProduct.service";
+import ListDepotCDService from "../services/depotCD/listDepotCD.service";
+import UpdateDepotCdProductService from "../services/depotCD/updateDepotCDProduct.service";
 
- 
 export default class DepotCDController {
-    static store = async(request: Request, response: Response) => {
-        const {product_id, cd_id, quantity} = request.body;
+  static store = async (request: Request, response: Response) => {
+    const { product_id, cd_id, quantity } = request.body;
 
-        const cdProduct = await AddProductService.execute({product_id, cd_id, quantity});
+    const cdProduct = await AddProductService.execute({
+      product_id,
+      cd_id,
+      quantity,
+    });
 
-        return response.status(201).json(cdProduct);
-    }
- 
-    static list = async(request: Request, response: Response) => {
-        const cdProducts = await ListDepotCDService.execute();
+    return response.status(201).json(cdProduct);
+  };
 
-        return response.json(cdProducts);
-    }
- 
-    static index = async(request: Request, response: Response) => {
-        const {id} = request.params;
+  static list = async (request: Request, response: Response) => {
+    const cdProducts = await ListDepotCDService.execute();
 
-        const cdProduct = await IndexDepotCDProductService.execute({id});
+    return response.json(cdProducts);
+  };
 
-        return response.json(cdProduct);
-    }
- 
-    static update = async(request: Request, response: Response) => {
-        const {id} = request.params;
-        const{ product_id, cd_id, quantity} = request.body;
+  static index = async (request: Request, response: Response) => {
+    const { id } = request.params;
 
-        const updatedCDProduct = await UpdateDepotCdProductService.execute({id, product_id, cd_id, quantity});
+    const cdProduct = await IndexDepotCDProductService.execute({ id });
 
-        return response.json(updatedCDProduct);
-    }
- 
-    static delete = async(request: Request, response: Response) => {
-        const {id} = request.params;
-        console.log('chegou aqui')
-        const deletedCDProduct = await DeleteDepotCDProductService.execute({id});
+    return response.json(cdProduct);
+  };
 
-        return response.status(204).json();
+  static update = async (request: Request, response: Response) => {
+    const { id } = request.params;
+    const { product_id, cd_id, quantity } = request.body;
 
-    }
- 
-};
+    const updatedCDProduct = await UpdateDepotCdProductService.execute({
+      id,
+      product_id,
+      cd_id,
+      quantity,
+    });
+
+    return response.json(updatedCDProduct);
+  };
+
+  static delete = async (request: Request, response: Response) => {
+    const { id } = request.params;
+    console.log("chegou aqui");
+    const deletedCDProduct = await DeleteDepotCDProductService.execute({ id });
+
+    return response.status(204).json();
+  };
+}
